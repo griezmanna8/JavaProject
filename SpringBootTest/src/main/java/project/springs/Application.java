c59051cd401f6d97bd4d9467b5be1d63c052e916
chenxi <chenxiliu@tencent.com>
2020-05-09 17:03:43 +0800

    1. 新增SpringBootTest文件。
    2. 增加pom文件。

---
+++ SpringBootTest/src/main/java/project/springs/Application.java
@@ -0,0 +1,31 @@
+package project.springs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @Author: chenxiliu
+ * @Descripion:
+ * @Date: Created in 2020/5/9
+ */
+@EnableScheduling
+@ComponentScan("project")
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication.run(Application.class, args);
+    }
+
+}
